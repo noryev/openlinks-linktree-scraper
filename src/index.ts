2adeb46e853d004eda8a99dbdeb8b2bf4c79d60a
Logan Lentz <logan.lentz1@gmail.com>
2022-11-02 16:00:44 +0000

    im tired

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,16 @@ import type { DataNode } from 'domhandler';
 
 export default function Scraper(profile: string): Promise<any> {
   return fetch('https://linktr.ee/' + profile)
-  .then((response: Response) => response.text())
-  .then((responseHtml: string) => {
-    try {
-      const $ = cheerio.load(responseHtml);
-      const data = (
-        ($('#__NEXT_DATA__')[0] as cheerio.NodeWithChildren
-      ).children[0] as unknown as DataNode).data;
-      return JSON.parse(data);
-    } catch (e) {
-      throw new Error('Unable to parse linktree config data');
-    }
-  });
+    .then((response: Response) => response.text())
+    .then((responseHtml: string) => {
+      try {
+        const $ = cheerio.load(responseHtml);
+        const data = (
+          ($('#__NEXT_DATA__')[0] as cheerio.NodeWithChildren
+          ).children[0] as unknown as DataNode).data;
+        return JSON.parse(data);
+      } catch (e) {
+        throw new Error('Unable to parse linktree config data');
+      }
+    });
 }
